68c9e9aa1f58c94a7450ee64ad03b076608ec326
kobulwidodo <fadhilhan01@gmail.com>
2022-06-06 23:50:05 +0700

    feat: add telemetry postgres layer

---
+++ telemetry/repository/postgresql/telemetry_postgresql.go
@@ -0,0 +1,38 @@
+package postgresql
+
+import (
+	"go-template/domain"
+
+	"gorm.io/gorm"
+)
+
+type TelemetryRepository struct {
+	db *gorm.DB
+}
+
+func NewTelemetryRepository(db *gorm.DB) domain.TelemetryRepository {
+	return &TelemetryRepository{db}
+}
+
+func (r *TelemetryRepository) Create(telemetry domain.Telemetry) error {
+	if err := r.db.Create(&telemetry).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *TelemetryRepository) GetAll() ([]domain.Telemetry, error) {
+	var telemetries []domain.Telemetry
+	if err := r.db.Find(&telemetries).Error; err != nil {
+		return telemetries, err
+	}
+	return telemetries, nil
+}
+
+func (r *TelemetryRepository) GetCount() ([]domain.CountResponse, error) {
+	var responses []domain.CountResponse
+	if err := r.db.Model(&domain.Telemetry{}).Select("endpoint", "count(*) as count", "count(DISTINCT ip) as unique_user").Group("endpoint").Find(&responses).Error; err != nil {
+		return responses, err
+	}
+	return responses, nil
+}
